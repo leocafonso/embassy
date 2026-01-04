318efebe39328d68dfe4f094ed146c2c365040b2
leocafonso <leocafonso@gmail.com>
2026-01-04 10:46:32 -0800

    fix warnings

--- embassy-ra/src/lib.rs
+++ embassy-ra/src/lib.rs
@@ -2,8 +2,18 @@
 
 pub use ra_metapac as pac;
 
+pub use embassy_hal_internal::{impl_peripheral, Peri as PeripheralRef, PeripheralType as Peripheral};
+
+pub mod interrupt;
+
+mod time_driver;
+
 pub mod peripherals {
     pub use ra_metapac::peripherals::*;
+}
+
+pub mod _macros {
+    pub use crate::bind_interrupts;
 }
 
 pub struct Config {
@@ -16,7 +26,18 @@ impl Default for Config {
     }
 }
 
+bind_interrupts!(pub struct Irqs {
+    Gpt0Ovf => time_driver::InterruptHandler<peripherals::GPT0>;
+    Gpt0Ccmpa => time_driver::InterruptHandler<peripherals::GPT0>;
+});
+
+pub static IRQS: Irqs = Irqs;
+
 pub fn init(_config: Config) -> peripherals::Peripherals {
-    // TODO: Initialize clocks, etc.
-    unsafe { peripherals::Peripherals::steal() }
+    critical_section::with(|cs| {
+        // TODO: Initialize clocks, etc.
+        // #[cfg(feature = "_time-driver")]
+        time_driver::init(cs);
+        unsafe { peripherals::Peripherals::steal() }
+    })
 }
